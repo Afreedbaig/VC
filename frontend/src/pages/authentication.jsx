acc7a9a0c8ade5939aa5357d76e6d42130a44d30
Afreedbaig <167954892+Afreedbaig@users.noreply.github.com>
2024-09-02 21:01:09 +0530

    Update authentication.jsx

--- frontend/src/pages/authentication.jsx
+++ frontend/src/pages/authentication.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState,useEffect } from "react";
 import "../App.css";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
